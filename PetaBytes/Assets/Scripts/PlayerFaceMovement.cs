5cedd965787144a7cd4ecea1b7d31000be33994f
Normanbg <normanbg98@gmail.com>
2019-01-21 17:59:42 +0100

    FaceMovement for the player

---
+++ PetaBytes/Assets/Scripts/PlayerFaceMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFaceMovement : MonoBehaviour {
+
+    PlayerMove playerMove;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerMove = GetComponent<PlayerMove>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float target_degrees = Mathf.Atan2(playerMove.movement.x, playerMove.movement.y) * Mathf.Rad2Deg;
+        transform.localRotation = Quaternion.AngleAxis(target_degrees, Vector3.back);
+    }
+}
