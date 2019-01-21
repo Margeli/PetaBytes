ab1e31f6888cab343b16b59b4406ecdc7e21c025
Normanbg <normanbg98@gmail.com>
2019-01-21 16:08:07 +0100

    FaceMovement WIP

--- PetaBytes/Assets/Scripts/KinematicFaceMovement.cs
+++ PetaBytes/Assets/Scripts/KinematicFaceMovement.cs
@@ -20,14 +20,5 @@ public class KinematicFaceMovement : SteeringAbstract
         float target_degrees = Mathf.Atan2(move.movement.x, move.movement.y) * Mathf.Rad2Deg;
         transform.localRotation = Quaternion.AngleAxis(target_degrees, Vector3.back);
 
-
-
-        float current_degrees = Mathf.Atan2(transform.forward.x, transform.forward.y) * Mathf.Rad2Deg;
-        float delta = Mathf.DeltaAngle(target_degrees, current_degrees);
-        if (Mathf.Abs(delta) < min_angle)
-            move.SetRotationVelocity(0.0f , priority);
-        else
-            move.SetRotationVelocity(-delta, priority);
-
     }
 }
